ac7528d91b37070f24b6f6aeaecaf4a386c0c496
nurikcat <221564@astanait.edu.kz>
2023-12-28 22:18:08 +0600

    feat: added pagination feature to divide it by 4 items

---
+++ js/pagination-script.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+    const articles = document.querySelectorAll('.my-project-item');
+    const itemsPerPage = 4;
+    const totalPages = Math.ceil(articles.length / itemsPerPage);
+    const pagination = document.getElementById('pageNumbers');
+    const paginationLinks = pagination.querySelectorAll('.page-link');
+
+    function showPage(pageNumber) {
+        const startIndex = (pageNumber - 1) * itemsPerPage;
+        const endIndex = startIndex + itemsPerPage;
+
+        articles.forEach((article, index) => {
+            if (index >= startIndex && index < endIndex) {
+                article.style.display = 'block';
+            } else {
+                article.style.display = 'none';
+            }
+        });
+
+        paginationLinks.forEach(link => {
+            link.classList.remove('active');
+        });
+
+        paginationLinks[pageNumber].classList.add('active');
+    }
+
+    showPage(1);
+
+    document.querySelectorAll('.page-link').forEach((link, index) => {
+        if (index > 0 && index <= totalPages) {
+            link.addEventListener('click', () => {
+                const pageNumber = parseInt(link.textContent);
+                showPage(pageNumber);
+            });
+        }
+    });
+})
