383f0379176c6d85df6cfdeef7571820c0992085
nyrta1 <nurtai_2005@mail.ru>
2024-01-25 16:36:06 +0600

    feat: added new projects

--- js/pagination-script.js
+++ js/pagination-script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     const articles = document.querySelectorAll('.my-project-item');
-    const itemsPerPage = 999;
+    const itemsPerPage = 4;
     const totalPages = Math.ceil(articles.length / itemsPerPage);
     const pagination = document.getElementById('pageNumbers');
     const paginationLinks = pagination.querySelectorAll('.page-link');
